a211e99c09cb96f8c66327f86e53ae109127b94a
Imran Rehman <imran.rehman@mail.utoronto.ca>
2022-10-24 00:35:25 -0400

    added logo and adjusted styling

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 export const LoginPage = () => {
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
   const register = async () => {};
 
   const login = async () => {};
